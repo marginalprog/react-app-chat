34911ecde7dde3e8b01004329a7ccd6b0138e55d
Ivan <korkishko.v@bk.ru>
2023-12-04 17:54:22 +0300

    Спринт 1. Каркас приложения,установка библиотек и пакетов

---
+++ src/components/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Main = () => {
+ return (
+  <main role="main">
+   <div className="py-5 bg-light">
+    <div className="container">
+      <h1>Hello world!</h1>
+    </div>
+    
+   </div>
+  </main>
+ );
+};
